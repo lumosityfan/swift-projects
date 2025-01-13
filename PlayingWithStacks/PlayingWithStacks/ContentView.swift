bbb9f9a11c1fbb55ed9253cf83688140b95b4010
Jeff Xie <37353357+lumosityfan@users.noreply.github.com>
2025-01-13 12:52:33 -0600

    Added button and gradient functionality
    Added buttons with specific alerts, multiple options as well as multiple types of gradients.

--- PlayingWithStacks/PlayingWithStacks/ContentView.swift
+++ PlayingWithStacks/PlayingWithStacks/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
     var body: some View {
         ZStack {
             VStack(spacing: 0){
@@ -17,8 +18,40 @@ struct ContentView: View {
             Text("your content")
                 .foregroundStyle(.secondary)
                 .padding(50)
-                .background(.ultraThinMaterial)
-        }.ignoresSafeArea()
+                .background(.green.gradient)
+        }
+        
+        LinearGradient(stops: [
+            .init(color: .white, location: 0.45),
+            .init(color: .black, location: 0.55),], startPoint: .top, endPoint: .bottom)
+        
+        RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
+        
+        Button("Delete selection", role: .destructive) {
+            print("Now deleting...")
+        }.buttonStyle(.borderedProminent)
+            .tint(.mint)
+        
+        Button("Show Alert") {
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("OK") {}
+            Button("Delete", role: .destructive) {}
+            Button("Cancel", role: .cancel) {}
+        } message: {
+        Text("Please read this.")
+        }
+        Button { showingAlert = true } label: {
+            Text("Tap me!")
+                .padding()
+                .foregroundStyle(.white)
+                .background(.red).alert("Button was tapped", isPresented: $showingAlert) {
+                    Button("OK") {}
+                }
+        }
+        
+        AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
     }
 }
 
